c621a495d465edc518a0c69781692e7a303786cb
Sonal <sonal.sharma@borrowell.com>
2025-02-21 16:49:42 -0500

    cleaning-code

--- AppleNewsUITestsUITests/AppleNewsUITestsUITests.swift
+++ AppleNewsUITestsUITests/AppleNewsUITestsUITests.swift
@@ -7,6 +7,16 @@ final class AppleNewsUITests: XCTestCase {
     override func setUpWithError() throws {
         continueAfterFailure = false
         app.launch()
+        
+        // Handle "Continue" button if it appears
+        let continueButton = app.buttons["Continue"]
+        if continueButton.waitForExistence(timeout: 5) {
+            if continueButton.isHittable {
+                continueButton.tap()
+            }
+        } else {
+            print("Continue button not found after 5 seconds.")
+        }
     }
     
     override func tearDownWithError() throws {
@@ -14,16 +24,6 @@ final class AppleNewsUITests: XCTestCase {
     }
     
     func testNavigationToTennisSportsNews() throws {
-        let continueButton = app.buttons["Continue"]
-        
-        // Wait for the button to appear
-        if continueButton.waitForExistence(timeout: 5) {
-                    if continueButton.isHittable {
-                        continueButton.tap()
-                    }
-                } else {
-                    print("Continue button not found after 5 seconds.")
-                }
         // Step 1: Verify ‘Today’ tab exists
         let todayTab = app.tabBars.buttons["Today"]
         XCTAssertTrue(todayTab.waitForExistence(timeout: 10), "Today tab not found!")
@@ -37,57 +37,36 @@ final class AppleNewsUITests: XCTestCase {
         let sportsMenuButton = app.buttons["Sections"]
         XCTAssertTrue(sportsMenuButton.waitForExistence(timeout: 10), "Sports menu button not found!")
             
-            
         // Step 4: Select Tennis from Sports Selection menu
-        sportsMenuButton.tap();
+        sportsMenuButton.tap()
         let tennisMenuOption = app.buttons["Tennis"]
-        tennisMenuOption.tap();
+        tennisMenuOption.tap()
         let tennisTitle = app.staticTexts["Tennis"]
         XCTAssertTrue(tennisTitle.waitForExistence(timeout: 5), "Tennis title doesn't exist")
-            
-        // Step 4: Scroll and click the ‘Follow Your Teams’ button
-        // Note: Commenting this because the label is not attached to the element, also Accessibility Inspector cannot detect any locator
-        // let followYourTeamsButton = app.buttons["Follow Your Teams"]
-            // while (!followYourTeamsButton.isHittable){
-            // app.swipeUp()
-            // }
-        // XCTAssertTrue(followYourTeamsButton.waitForExistence(timeout: 5), "'Follow Your Teams' button not found!")
-        //  followYourTeamsButton.tap()
     }
 
     func testDiscoverMoreChannels() throws {
-        let continueButton = app.buttons["Continue"]
-        
-        // Wait for the button to appear
-        if continueButton.waitForExistence(timeout: 5) {
-                    if continueButton.isHittable {
-                        continueButton.tap()
-                    }
-                } else {
-                    print("Continue button not found after 5 seconds.")
-                }
-        
         // Step 1: Verify ‘Following’ tab exists
         let followingTab = app.tabBars.buttons["Following"]
         XCTAssertTrue(followingTab.waitForExistence(timeout: 5), "Following tab not found!")
-        followingTab.tap();
+        followingTab.tap()
             
         // Step 2: Scroll to the ‘Discover’ Button
         let discoverChannelsButton = app.buttons["Discover Channels"]
-        while (!discoverChannelsButton.isHittable){
-                 app.swipeUp()
-               }
+        while (!discoverChannelsButton.isHittable) {
+            app.swipeUp()
+        }
             
         XCTAssertTrue(discoverChannelsButton.waitForExistence(timeout: 10), "Discover button not found!")
         discoverChannelsButton.tap()
             
-        // Step 3: Verify user can search Ottawa Citizen channel'
+        // Step 3: Verify user can search Ottawa Citizen channel
         let searchField = app.searchFields["Search"]
         searchField.tap()
         searchField.typeText("Ottawa Citizen")
         searchField.typeText("\n")
         let channel = app.otherElements.containing(NSPredicate(format: "label CONTAINS %@", "Ottawa Citizen")).element
         XCTAssertTrue(channel.waitForExistence(timeout: 10), "Channel containing 'Ottawa Citizen' was not found!")
-
     }
 }
+
