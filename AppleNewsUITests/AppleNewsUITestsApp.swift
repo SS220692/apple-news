8db6fd107688cfc6d8983df9c0aa4887f1adf87d
Sonal <sonal.sharma@borrowell.com>
2025-02-21 12:27:45 -0500

    Initial Commit

---
+++ AppleNewsUITests/AppleNewsUITestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleNewsUITestsApp.swift
+//  AppleNewsUITests
+//
+//  Created by Sonal Sharma on 2025-02-21.
+//
+
+import SwiftUI
+
+@main
+struct AppleNewsUITestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
